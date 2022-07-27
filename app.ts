ac53622366c9a7efd146651ba37ce832a9317a71
Tomasz Rohda <tomaszrohda@wp.pl>
2022-07-27 15:28:13 +0200

    Never type

--- app.ts
+++ app.ts
@@ -9,3 +9,8 @@ userInput = "Tomek";
 if (typeof userInput === "string") {
   userName = userInput;
 }
+
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+}
+generateError("An error occurred!", 500);
