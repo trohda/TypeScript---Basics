3da3260335b9395cc29d8ae747ae4b4900334eea
Tomasz Rohda <tomaszrohda@wp.pl>
2022-07-27 09:55:17 +0200

    Test Tuple type

--- app.js
+++ app.js
@@ -5,11 +5,14 @@
 var person = {
     name: "Tomek",
     age: 40,
-    hobbies: ["sports", "cooking"]
+    hobbies: ["sports", "cooking"],
+    role: [2, "author"]
 };
+person.role.push("admin");
+// person.role[1] = 10;
 var favoriteActivities;
 favoriteActivities = ["sports"];
-console.log(person.name);
+console.log(person.role);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase);
--- app.ts
+++ app.ts
@@ -2,14 +2,24 @@
 //   name: string;
 //   age: number;
 // } = {
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: "Tomek",
   age: 40,
   hobbies: ["sports", "cooking"],
+  role: [2, "author"],
 };
 
-let favoriteActivities: string[];
+// person.role.push("admin");
+// person.role[1] = 10;
 
+// person.role = [0, "admin", 'user']; !!!ERROR !!!
+
+let favoriteActivities: string[];
 favoriteActivities = ["sports"];
 
 console.log(person.name);
