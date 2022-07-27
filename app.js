2377a782c4db66fda71b973c937248e0746e5f50
Tomasz Rohda <tomaszrohda@wp.pl>
2022-07-27 10:35:35 +0200

    Test Enum type

--- app.js
+++ app.js
@@ -2,18 +2,42 @@
 //   name: string;
 //   age: number;
 // } = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Tomek",
+//   age: 40,
+//   hobbies: ["sports", "cooking"],
+//   role: [2, "author"],
+// };
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: "Tomek",
     age: 40,
     hobbies: ["sports", "cooking"],
-    role: [2, "author"]
+    role: Role.ADMIN
 };
-person.role.push("admin");
+// person.role.push("admin");
 // person.role[1] = 10;
+// person.role = [0, "admin", 'user']; !!!ERROR !!!
 var favoriteActivities;
 favoriteActivities = ["sports"];
-console.log(person.role);
+console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
-    console.log(hobby.toUpperCase);
+    console.log(hobby);
+}
+if (person.role === Role.AUTHOR) {
+    console.log("is author");
 }
--- app.ts
+++ app.ts
@@ -2,21 +2,37 @@
 //   name: string;
 //   age: number;
 // } = {
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Tomek",
+//   age: 40,
+//   hobbies: ["sports", "cooking"],
+//   role: [2, "author"],
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
   name: "Tomek",
   age: 40,
   hobbies: ["sports", "cooking"],
-  role: [2, "author"],
+  role: Role.ADMIN,
 };
 
 // person.role.push("admin");
 // person.role[1] = 10;
-
 // person.role = [0, "admin", 'user']; !!!ERROR !!!
 
 let favoriteActivities: string[];
@@ -25,5 +41,9 @@ favoriteActivities = ["sports"];
 console.log(person.name);
 
 for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase);
+  console.log(hobby);
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log("is author");
 }
