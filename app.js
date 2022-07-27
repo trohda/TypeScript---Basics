ca697458fcec731575c36d7ef4ce7ddc04e5fb55
Tomasz Rohda <tomaszrohda@wp.pl>
2022-07-27 09:31:44 +0200

    Test basic array type

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
 var person = {
     name: "Tomek",
-    age: 40
+    age: 40,
+    hobbies: ["sports", "cooking"]
 };
-console.log(person.nickname);
+var favoriteActivities;
+favoriteActivities = ["sports"];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase);
+}
--- app.ts
+++ app.ts
@@ -5,6 +5,15 @@
 const person = {
   name: "Tomek",
   age: 40,
+  hobbies: ["sports", "cooking"],
 };
 
+let favoriteActivities: string[];
+
+favoriteActivities = ["sports"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase);
+}
